84f6207a42d15b989c7a613075e1e23ca6093672
praingeard <paul.raingeard@gmail.com>
2019-12-08 15:58:09 +0100

    bin dir deleted

--- bin/targets/m68k-kick13/include/clib/potgo_protos.h
+++
@@ -1,9 +0,0 @@
-/* struct Library */
-#ifndef EXEC_LIBRARIES_H
-#include <exec/libraries.h>
-#endif
-
-extern struct Library *PotgoBase;
-long AllocPotBits(long);
-void FreePotBits(long);
-void WritePotgo(long, long);
--- bin/targets/m68k-kick13/include/clib/translator_protos.h
+++
@@ -1,7 +0,0 @@
-/* struct Library */
-#ifndef EXEC_LIBRARIES_H
-#include <exec/libraries.h>
-#endif
-
-extern struct Library *TranslatorBase;
-long Translate(char *, long, char *, long);
